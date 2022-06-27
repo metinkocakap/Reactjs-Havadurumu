633bd71c15694a51e3c0ce7331b3255d47df13c4
Metin Kocakap <kocakap86@gmail.com>
2022-06-27 19:00:52 +0300

    completed

---
+++ src/Components/Background/index.js
@@ -0,0 +1,12 @@
+import CheckPicture from "./CheckPicture"
+
+function index({back}) {
+  return (
+    <div className='back'>
+      <CheckPicture durum={back}/>
+     
+    </div>
+  )
+}
+
+export default index
